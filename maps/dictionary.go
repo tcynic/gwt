2380645b17f18de9247f9a6847e143f7169eccc0
Terrence Cort <tchriscort@gmail.com>
2020-04-26 21:04:20 -0400

    adding the examples for the maps chapter

---
+++ maps/dictionary.go
@@ -0,0 +1,60 @@
+package main
+
+// Dictionary is a type map
+type Dictionary map[string]string
+
+type DictionaryErr string
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it doesn't exist")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+// Search looks for a word in the dictionary and returns the definition
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+// Add adds a value to the dictionary returns and error
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
