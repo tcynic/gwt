ea0b687871793b7a07df0282fd85f21964b5ca02
Terrence Cort <tchriscort@gmail.com>
2020-06-04 14:52:27 -0400

    v1 of code

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -15,6 +15,10 @@ func TestWalk(t *testing.T) {
 		got = append(got, input)
 	})
 
+	if got[0] != expected {
+		t.Errorf("got %q, want %q", got[0], expected)
+	}
+
 	if len(got) != 1 {
 		t.Errorf("wrong number of function calls, get %d want %d", len(got), 1)
 	}
