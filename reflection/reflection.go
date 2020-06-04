428596d6b5d99b940ebe59e3c3ba4790b1d2783e
Terrence Cort <tchriscort@gmail.com>
2020-06-04 17:01:32 -0400

    finished chapter'

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,25 +1,34 @@
 package main
 
-import "reflect"
+import (
+	"reflect"
+)
 
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	if val.Kind() == reflect.Slice {
-		for i := 0; i < val.Len(); i++ {
-			walk(val.Index(i).Interface(), fn)
-		}
-		return
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
 	}
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walk(v.Interface(), fn)
 		}
 	}
 }
--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -5,16 +5,6 @@ import (
 	"testing"
 )
 
-type Person struct {
-	Name    string
-	Profile Profile
-}
-
-type Profile struct {
-	Age  int
-	City string
-}
-
 func TestWalk(t *testing.T) {
 
 	cases := []struct {
@@ -24,16 +14,14 @@ func TestWalk(t *testing.T) {
 	}{
 		{
 			"Struct with one string field",
-			struct {
-				Name string
-			}{"Chris"},
+			struct{ Name string }{"Chris"},
 			[]string{"Chris"},
 		},
 		{
-			"Struct with two strings",
+			"Struct with two string fields",
 			struct {
 				Name string
-				city string
+				City string
 			}{"Chris", "London"},
 			[]string{"Chris", "London"},
 		},
@@ -41,7 +29,7 @@ func TestWalk(t *testing.T) {
 			"Struct with non string field",
 			struct {
 				Name string
-				age  int
+				Age  int
 			}{"Chris", 33},
 			[]string{"Chris"},
 		},
@@ -65,9 +53,17 @@ func TestWalk(t *testing.T) {
 			"Slices",
 			[]Profile{
 				{33, "London"},
-				{34, "Reykjavik"},
+				{34, "Reykjavík"},
 			},
-			[]string{"London", "Reykjavik"},
+			[]string{"London", "Reykjavík"},
+		},
+		{
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
 		},
 	}
 
@@ -79,8 +75,66 @@ func TestWalk(t *testing.T) {
 			})
 
 			if !reflect.DeepEqual(got, test.ExpectedCalls) {
-				t.Errorf("got %v want %v", got, test.ExpectedCalls)
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
 			}
 		})
+	}
+
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "Berlin"}
+			aChannel <- Profile{34, "Katowice"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %+v to contain %q but it didn't", haystack, needle)
 	}
 }
